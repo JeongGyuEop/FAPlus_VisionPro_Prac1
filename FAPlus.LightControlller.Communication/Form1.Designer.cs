1608069e17850ed565d7e279cae3c23279adef99
JeongGyuEop <gueeop0219@naver.com>
2025-07-22 17:22:54 +0900

    Light Controller Ehternet & RS-232 Communication

--- FAPlus.LightControlller.Communication/Form1.Designer.cs
+++ FAPlus.LightControlller.Communication/Form1.Designer.cs
@@ -28,13 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.OnButton = new System.Windows.Forms.RadioButton();
+            this.OffButton = new System.Windows.Forms.RadioButton();
+            this.Ch_1 = new System.Windows.Forms.CheckBox();
+            this.Ch_2 = new System.Windows.Forms.CheckBox();
+            this.Ch_3 = new System.Windows.Forms.CheckBox();
+            this.Ch_4 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // OnButton
+            // 
+            this.OnButton.AutoSize = true;
+            this.OnButton.Location = new System.Drawing.Point(29, 218);
+            this.OnButton.Name = "OnButton";
+            this.OnButton.Size = new System.Drawing.Size(41, 16);
+            this.OnButton.TabIndex = 0;
+            this.OnButton.TabStop = true;
+            this.OnButton.Text = "ON";
+            this.OnButton.UseVisualStyleBackColor = true;
+            this.OnButton.CheckedChanged += new System.EventHandler(this.OnButton_CheckedChanged);
+            // 
+            // OffButton
+            // 
+            this.OffButton.AutoSize = true;
+            this.OffButton.Location = new System.Drawing.Point(91, 218);
+            this.OffButton.Name = "OffButton";
+            this.OffButton.Size = new System.Drawing.Size(46, 16);
+            this.OffButton.TabIndex = 1;
+            this.OffButton.TabStop = true;
+            this.OffButton.Text = "OFF";
+            this.OffButton.UseVisualStyleBackColor = true;
+            this.OffButton.CheckedChanged += new System.EventHandler(this.OffButton_CheckedChanged);
+            // 
+            // Ch_1
+            // 
+            this.Ch_1.AutoSize = true;
+            this.Ch_1.Location = new System.Drawing.Point(29, 281);
+            this.Ch_1.Name = "Ch_1";
+            this.Ch_1.Size = new System.Drawing.Size(81, 16);
+            this.Ch_1.TabIndex = 2;
+            this.Ch_1.Text = "Channel 1";
+            this.Ch_1.UseVisualStyleBackColor = true;
+            this.Ch_1.UseWaitCursor = true;
+            // 
+            // Ch_2
+            // 
+            this.Ch_2.AutoSize = true;
+            this.Ch_2.Location = new System.Drawing.Point(141, 280);
+            this.Ch_2.Name = "Ch_2";
+            this.Ch_2.Size = new System.Drawing.Size(81, 16);
+            this.Ch_2.TabIndex = 3;
+            this.Ch_2.Text = "Channel 2";
+            this.Ch_2.UseVisualStyleBackColor = true;
+            // 
+            // Ch_3
+            // 
+            this.Ch_3.AutoSize = true;
+            this.Ch_3.Location = new System.Drawing.Point(29, 303);
+            this.Ch_3.Name = "Ch_3";
+            this.Ch_3.Size = new System.Drawing.Size(81, 16);
+            this.Ch_3.TabIndex = 4;
+            this.Ch_3.Text = "Channel 3";
+            this.Ch_3.UseVisualStyleBackColor = true;
+            // 
+            // Ch_4
+            // 
+            this.Ch_4.AutoSize = true;
+            this.Ch_4.Location = new System.Drawing.Point(141, 302);
+            this.Ch_4.Name = "Ch_4";
+            this.Ch_4.Size = new System.Drawing.Size(81, 16);
+            this.Ch_4.TabIndex = 5;
+            this.Ch_4.Text = "Channel 4";
+            this.Ch_4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 192);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "※ Light ON/OFF";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 257);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "※ Light Channel";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Ch_4);
+            this.Controls.Add(this.Ch_3);
+            this.Controls.Add(this.Ch_2);
+            this.Controls.Add(this.Ch_1);
+            this.Controls.Add(this.OffButton);
+            this.Controls.Add(this.OnButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton OnButton;
+        private System.Windows.Forms.RadioButton OffButton;
+        private System.Windows.Forms.CheckBox Ch_1;
+        private System.Windows.Forms.CheckBox Ch_2;
+        private System.Windows.Forms.CheckBox Ch_3;
+        private System.Windows.Forms.CheckBox Ch_4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- FAPlus.LightControlller.Communication/Form1.cs
+++ FAPlus.LightControlller.Communication/Form1.cs
@@ -1,20 +1,144 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace FAPlus.LightControlller.Communication
 {
     public partial class Form1 : Form
     {
+        byte start = 0x01;
+        byte op_code;
+        byte data_length;
+        byte address;
+        byte data;
+        byte end = 0x04;
+
+        byte[] buffer;
+
+        // 1. 소켓 생성
+        Socket socket; // Ethernet
+        IPEndPoint endPoint; // Ethernet
+
+        SerialPort serialPort; //RS-232
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Socket_Connect();
+            //RS232_Connect(); // RS-232
+        }
+
+        private void Socket_Connect()
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            endPoint = new IPEndPoint(IPAddress.Parse("192.168.0.230"), 50000);
+
+        }
+
+        private void RS232_Connect()
+        {
+            //serialPort = new SerialPort("COM1", 19200, Parity.None, 8, StopBits.One); // RS-232
+            //serialPort.Open();// RS-232
+        }
+
+        private void Receive_Call()
+        {
+            byte[] response = new byte[10];
+            int length = socket.Receive(response); // 응답 수신 (예: 0x06)
+
+            Console.WriteLine("수신된 바이트 수: " + length);
+            Console.WriteLine("받은 데이터(hex): " + response[0].ToString("X2"));
+        }
+
+        private void OnButton_CheckedChanged(object sender, EventArgs e)
+        {
+            buffer = new byte[]
+            {
+                start,       // Start
+                0x00,       // OP (Write)
+                0x01,       // DL
+                0x34,       // Addr (Output Enable 주소 예시)
+                0x01,       // Data (1 = Enable)
+                end        // End
+            };
+
+            socket.SendTo(buffer, endPoint); // Ethernet
+            //serialPort.Write(buffer, 0, buffer.Length); // RS-232
+
+            Receive_Call(); // 응답 수신
+        }
+
+        private void OffButton_CheckedChanged(object sender, EventArgs e)
+        {
+            buffer = new byte[]
+            {
+                start,       // Start
+                0x00,       // OP (Write)
+                0x01,       // DL
+                0x34,       // Addr (Output Enable 주소 예시)
+                0x02,       // Data (1 = Enable)
+                end        // End
+            };              
+
+            socket.SendTo(buffer, endPoint); // Ethernet
+            //serialPort.Write(buffer, 0, buffer.Length); // RS-232
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (socket != null) socket.Close();
+            if (serialPort != null && serialPort.IsOpen) serialPort.Close();
+        }
+        //// 1. CSR 설정 패킷 (채널 선택)
+        //buffer = new byte[]
+        //{
+        //    start,
+        //    0x00,       // OP
+        //    0x01,       // DL
+        //    0x20,       // Addr
+        //    0x01,       // Data
+        //    end        // End
+        //};
+
+        //// 2. SVR 설정 패킷 (밝기 설정)
+        //byte[] svrPacket = new byte[]
+        //{
+        //    0x01,       // Start
+        //    0x00,       // OP
+        //    0x01,       // DL
+        //    0x28,       // Addr
+        //    0xFF,       // Data
+        //    0x04        // End
+        //};
+
+        //try
+        //{
+        //    socket.SendTo(buffer, endPoint);
+        //    Console.WriteLine("CSR 전송 완료");
+
+        //    Thread.Sleep(50); // 잠깐 대기 (필요시)
+
+        //    socket.SendTo(svrPacket, endPoint);
+        //    Console.WriteLine("SVR 전송 완료");
+
+        // 출력 ON (Output Enable 명령 필요 시)
+        //socket.SendTo(enablePacket, endPoint);
+
+
+        //}
+        //catch (Exception ex)
+        //{
+        //    Console.WriteLine("전송 오류: " + ex.Message);
+        //}
     }
 }
+   
