31e116ad41e54ba4413010e4e40c0b383adae8b2
JeongGyuEop <gueeop0219@naver.com>
2025-08-05 13:21:12 +0900

    Camera Connect & MainForm Show -ing

---
+++ FAPlus.AquisitionCamera/AquisitionCameraForm.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace FAPlus.AquisitionCamera
+{
+    partial class AquisitionCameraForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AquisitionCameraForm));
+            this.cogDisplay1 = new Cognex.VisionPro.Display.CogDisplay();
+            this.Acq_Once = new System.Windows.Forms.Button();
+            this.BoardTypeLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.VideoFormatCombo = new System.Windows.Forms.ComboBox();
+            this.brightnessUpDown = new System.Windows.Forms.NumericUpDown();
+            this.contrastUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblNoBrightness = new System.Windows.Forms.Label();
+            this.lblNoContrast = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblNoExposure = new System.Windows.Forms.Label();
+            this.exposureUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.cogDisplay1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.brightnessUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.contrastUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exposureUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cogDisplay1
+            // 
+            this.cogDisplay1.ColorMapLowerClipColor = System.Drawing.Color.Black;
+            this.cogDisplay1.ColorMapLowerRoiLimit = 0D;
+            this.cogDisplay1.ColorMapPredefined = Cognex.VisionPro.Display.CogDisplayColorMapPredefinedConstants.None;
+            this.cogDisplay1.ColorMapUpperClipColor = System.Drawing.Color.Black;
+            this.cogDisplay1.ColorMapUpperRoiLimit = 1D;
+            this.cogDisplay1.DoubleTapZoomCycleLength = 2;
+            this.cogDisplay1.DoubleTapZoomSensitivity = 2.5D;
+            this.cogDisplay1.Location = new System.Drawing.Point(394, 12);
+            this.cogDisplay1.MouseWheelMode = Cognex.VisionPro.Display.CogDisplayMouseWheelModeConstants.Zoom1;
+            this.cogDisplay1.MouseWheelSensitivity = 1D;
+            this.cogDisplay1.Name = "cogDisplay1";
+            this.cogDisplay1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("cogDisplay1.OcxState")));
+            this.cogDisplay1.Size = new System.Drawing.Size(394, 366);
+            this.cogDisplay1.TabIndex = 0;
+            // 
+            // Acq_Once
+            // 
+            this.Acq_Once.Location = new System.Drawing.Point(394, 384);
+            this.Acq_Once.Name = "Acq_Once";
+            this.Acq_Once.Size = new System.Drawing.Size(188, 34);
+            this.Acq_Once.TabIndex = 1;
+            this.Acq_Once.Text = "1회 촬영";
+            this.Acq_Once.UseVisualStyleBackColor = true;
+            this.Acq_Once.Click += new System.EventHandler(this.AcqOnce_Click);
+            // 
+            // BoardTypeLabel
+            // 
+            this.BoardTypeLabel.AutoSize = true;
+            this.BoardTypeLabel.Location = new System.Drawing.Point(14, 59);
+            this.BoardTypeLabel.Name = "BoardTypeLabel";
+            this.BoardTypeLabel.Size = new System.Drawing.Size(17, 12);
+            this.BoardTypeLabel.TabIndex = 4;
+            this.BoardTypeLabel.Text = " : ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "FrameGrabber";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Video Format :";
+            // 
+            // VideoFormatCombo
+            // 
+            this.VideoFormatCombo.FormattingEnabled = true;
+            this.VideoFormatCombo.Location = new System.Drawing.Point(14, 101);
+            this.VideoFormatCombo.Name = "VideoFormatCombo";
+            this.VideoFormatCombo.Size = new System.Drawing.Size(357, 20);
+            this.VideoFormatCombo.TabIndex = 7;
+            this.VideoFormatCombo.SelectedIndexChanged += new System.EventHandler(this.VideoFormatCombo_SelectedIndexChanged);
+            // 
+            // brightnessUpDown
+            // 
+            this.brightnessUpDown.DecimalPlaces = 3;
+            this.brightnessUpDown.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            196608});
+            this.brightnessUpDown.Location = new System.Drawing.Point(131, 183);
+            this.brightnessUpDown.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.brightnessUpDown.Name = "brightnessUpDown";
+            this.brightnessUpDown.Size = new System.Drawing.Size(156, 21);
+            this.brightnessUpDown.TabIndex = 8;
+            this.brightnessUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.brightnessUpDown.ValueChanged += new System.EventHandler(this.brightnessUpDown_ValueChanged);
+            // 
+            // contrastUpDown
+            // 
+            this.contrastUpDown.DecimalPlaces = 3;
+            this.contrastUpDown.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.contrastUpDown.Location = new System.Drawing.Point(131, 219);
+            this.contrastUpDown.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.contrastUpDown.Name = "contrastUpDown";
+            this.contrastUpDown.Size = new System.Drawing.Size(156, 21);
+            this.contrastUpDown.TabIndex = 9;
+            this.contrastUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.contrastUpDown.ValueChanged += new System.EventHandler(this.contrastUpDown_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Brightness : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(67, 224);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 12);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Contrast : ";
+            // 
+            // lblNoBrightness
+            // 
+            this.lblNoBrightness.AutoSize = true;
+            this.lblNoBrightness.Location = new System.Drawing.Point(59, 188);
+            this.lblNoBrightness.Name = "lblNoBrightness";
+            this.lblNoBrightness.Size = new System.Drawing.Size(228, 12);
+            this.lblNoBrightness.TabIndex = 22;
+            this.lblNoBrightness.Text = "CogAcqFifo Doesn\'t Support Brightness";
+            this.lblNoBrightness.Visible = false;
+            // 
+            // lblNoContrast
+            // 
+            this.lblNoContrast.AutoSize = true;
+            this.lblNoContrast.Location = new System.Drawing.Point(67, 224);
+            this.lblNoContrast.Name = "lblNoContrast";
+            this.lblNoContrast.Size = new System.Drawing.Size(215, 12);
+            this.lblNoContrast.TabIndex = 23;
+            this.lblNoContrast.Text = "CogAcqFifo Doesn\'t Support Contrast";
+            this.lblNoContrast.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(293, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 12);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "ms";
+            // 
+            // lblNoExposure
+            // 
+            this.lblNoExposure.AutoSize = true;
+            this.lblNoExposure.Location = new System.Drawing.Point(60, 150);
+            this.lblNoExposure.Name = "lblNoExposure";
+            this.lblNoExposure.Size = new System.Drawing.Size(71, 12);
+            this.lblNoExposure.TabIndex = 25;
+            this.lblNoExposure.Text = "Exposure : ";
+            // 
+            // exposureUpDown
+            // 
+            this.exposureUpDown.DecimalPlaces = 2;
+            this.exposureUpDown.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.exposureUpDown.Location = new System.Drawing.Point(131, 146);
+            this.exposureUpDown.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.exposureUpDown.Name = "exposureUpDown";
+            this.exposureUpDown.Size = new System.Drawing.Size(156, 21);
+            this.exposureUpDown.TabIndex = 26;
+            this.exposureUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.exposureUpDown.ValueChanged += new System.EventHandler(this.exposureUpDown_ValueChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(67, 150);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(222, 12);
+            this.label7.TabIndex = 27;
+            this.label7.Text = "CogAcqFifo Doesn\'t Support Exposure";
+            this.label7.Visible = false;
+            // 
+            // AquisitionCameraForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 440);
+            this.Controls.Add(this.exposureUpDown);
+            this.Controls.Add(this.lblNoExposure);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.contrastUpDown);
+            this.Controls.Add(this.brightnessUpDown);
+            this.Controls.Add(this.VideoFormatCombo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BoardTypeLabel);
+            this.Controls.Add(this.Acq_Once);
+            this.Controls.Add(this.cogDisplay1);
+            this.Controls.Add(this.lblNoBrightness);
+            this.Controls.Add(this.lblNoContrast);
+            this.Controls.Add(this.label7);
+            this.Name = "AquisitionCameraForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.cogDisplay1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.brightnessUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.contrastUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exposureUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Cognex.VisionPro.Display.CogDisplay cogDisplay1;
+        private System.Windows.Forms.Button Acq_Once;
+        private System.Windows.Forms.Label BoardTypeLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox VideoFormatCombo;
+        private System.Windows.Forms.NumericUpDown brightnessUpDown;
+        private System.Windows.Forms.NumericUpDown contrastUpDown;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        internal System.Windows.Forms.Label lblNoBrightness;
+        internal System.Windows.Forms.Label lblNoContrast;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblNoExposure;
+        private System.Windows.Forms.NumericUpDown exposureUpDown;
+        internal System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ FAPlus.AquisitionCamera/AquisitionCameraForm.cs
@@ -0,0 +1,324 @@
+﻿using Cognex.VisionPro;
+using Cognex.VisionPro.Display;
+using Cognex.VisionPro.Exceptions;
+using System;
+using System.Windows.Forms;
+
+namespace FAPlus.AquisitionCamera
+{
+    public partial class AquisitionCameraForm : Form
+    {
+        private CogAcqFifoTool fifoTool;               // VisionPro에서 이미지 획득을 위한 고수준 도구형 객체 (내부에 ICogAcqFifo(Operator)포함)
+        private ICogFrameGrabber mFrameGrabber = null; // 하드웨어 카메라 보드와 연결을 담당하는 프레임그래버 객체
+        private ICogAcqFifo mAcqFifo = null;           // 카메라에서 이미지를 획득하는 저수준 획득 객체
+        private int acqCount = 0;                      // 이미지 획득 횟수 기록 (GC 트리거용)
+        private ICogAcqBrightness mBrightness;         // 카메라의 밝기(Brightness) 파라미터 제어 객체
+        private ICogAcqContrast mContrast;             // 카메라의 대비(Contrast) 파라미터 제어 객체
+        private ICogAcqExposure mExposure;             // 카메라의 노출 시간(Exposure Time)을 제어하는 객체
+        private string selectedFormat = "";            // 이전에 선택했던 비디오 포맷 저장용
+
+        public AquisitionCameraForm()
+        {
+            //this.FormClosing += Form1_FormClosing;
+            //this.FormClosed += Form1_FormClosed;
+        }  // 기본 생성자
+
+        public AquisitionCameraForm(bool init)
+        {
+            InitializeComponent(); // WinForm UI 구성
+            InitializeAcquisition(init);
+        } // 폼 로드 시 초기화
+
+        private void InitializeAcquisition(bool init)
+        {
+            try
+            {
+                if (init) // 만약 처음 창이 열리는 것이면
+                {
+                    CogFrameGrabbers mFrameGrabbers = new CogFrameGrabbers(); // 현재 시스템에 연결된 모든 프레임그래버 목록 가져옴
+                    mFrameGrabber = mFrameGrabbers[0]; // 첫 번째 프레임그래버를 사용하여 객체 초기화
+
+                    fifoTool = new CogAcqFifoTool(); // 이미지 획득용 도구 객체 생성 (고수준)
+                    mAcqFifo = fifoTool.Operator; // 내부 Operator(ICogAcqFifo) 획득기 설정
+
+                    // 내부 Operator가 null 이거나, 프레임그래버가 없으면 예외처리
+                    if (mFrameGrabbers.Count < 1) 
+                        throw new CogAcqNoFrameGrabberException("프레임그래버를 찾을 수 없습니다.");
+
+                    BoardTypeLabel.Text = mAcqFifo.FrameGrabber.Name; // 연결된 프레임그래버(카메라보드) 이름 표시
+
+                    // 사용 가능한 비디오 포맷 목록을 콤보박스에 추가
+                    VideoFormatCombo.Items.Clear();
+                    foreach (string format in mFrameGrabber.AvailableVideoFormats)
+                        VideoFormatCombo.Items.Add(format);
+
+                    // 첫 번째 포맷 선택
+                    selectedFormat = mFrameGrabber.AvailableVideoFormats[0];
+
+                    // 초기 UI 상태 설정
+                    SetControlEnabled(false);
+                } 
+                else // 카메라 연결 Form이 2번 이상 열린 경우
+                {
+                    VideoFormatCombo.SelectedItem = selectedFormat;
+
+                    // 초기 UI 상태 설정
+                    SetControlEnabled(true);
+                }
+
+
+                // 콤보박스 선택 이벤트 등록
+                VideoFormatCombo.SelectedIndexChanged -= VideoFormatCombo_SelectedIndexChanged;
+                VideoFormatCombo.SelectedIndexChanged += VideoFormatCombo_SelectedIndexChanged;
+
+                // Fifo 생성 및 UI 설정
+                CreateAcqFifoWithParams(selectedFormat);
+                UpdateCameraParamsUI();
+
+            }
+            catch (CogAcqException ex)
+            {
+                MessageBox.Show("카메라가 연결되지 않았습니다: " + ex.Message);
+            }
+
+        } // 프로그램 시작 시 카메라 연결 및 초기화
+
+        private void SetControlEnabled(bool enabled)
+        {
+            exposureUpDown.Enabled = enabled;
+            brightnessUpDown.Enabled = enabled;
+            contrastUpDown.Enabled = enabled;
+        } // 코드 중복 제거
+
+        private void CreateAcqFifoWithParams(string videoFormat)
+        {
+            try
+            {
+                // LiveDisplay 중지
+                if (cogDisplay1.LiveDisplayRunning)
+                {
+                    cogDisplay1.StopLiveDisplay();
+                }
+
+                // 기존 AcqFifo가 있다면 해제
+                if (mAcqFifo is IDisposable disposable)
+                {
+                    disposable.Dispose();
+                    mAcqFifo = null;
+                }
+
+                // 새 FIFO 생성
+                mAcqFifo = mFrameGrabber.CreateAcqFifo(
+                    videoFormat,
+                    CogAcqFifoPixelFormatConstants.Format8Grey,
+                    0,
+                    true
+                );
+            }
+            catch (CogAcqCannotCreateFifoException ex)
+            {
+                MessageBox.Show("FIFO 생성 실패: " + ex.Message);
+            }
+        }
+
+        private void UpdateCameraParamsUI()
+        {
+            // ----- 노출 값 설정 -----
+            // 노출 설정 불가능할 경우 라벨로 알림 표시
+            mExposure = mAcqFifo.OwnedExposureParams; // 노출 파라미터 객체 얻기
+            if (mExposure != null)
+            {
+                exposureUpDown.Value = (decimal)mExposure.Exposure;
+            }
+            else
+            {
+                exposureUpDown.Visible = false;
+                lblNoExposure.Visible = false;
+            }
+
+            // ----- 밝기 값 설정 -----
+            // 밝기 설정 불가능할 경우 라벨로 알림 표시
+            mBrightness = mAcqFifo.OwnedBrightnessParams; // 밝기 파라미터 객체 얻기
+            if (mBrightness != null)
+            {
+                brightnessUpDown.Value = (decimal)mBrightness.Brightness;
+            }
+            else
+            {
+                brightnessUpDown.Visible = false;
+                lblNoBrightness.Visible = true;
+            }
+
+            // ----- 대비 값 설정 -----
+            // 대비 설정 불가능할 경우 라벨로 알림 표시
+            mContrast = mAcqFifo.OwnedContrastParams; // 대비 파라미터 객체 얻기
+            if (mContrast != null)
+            {
+                contrastUpDown.Value = (decimal)mContrast.Contrast;
+            }
+            else
+            {
+                contrastUpDown.Visible = false;
+                lblNoContrast.Visible = true;
+            }
+        }
+
+
+        //==================================================================================================
+        // 촬영 이벤트
+        private void AcqOnce_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 라이브 디스플레이가 남아있지 않은 상태여야 안전하게 해제 가능
+                if (cogDisplay1.LiveDisplayRunning) cogDisplay1.StopLiveDisplay();
+
+                // 기존 AcqFifo가 있다면 해제
+                (mAcqFifo as IDisposable)?.Dispose();
+
+                // 사용 가능한 비디오 포맷 중 첫 번째 선택
+                string videoFormat = mFrameGrabber.AvailableVideoFormats[0];
+                CreateAcqFifoWithParams(videoFormat);
+
+                // 이미지 획득 및 디스플레이에 표시
+                int triggerNum;
+                cogDisplay1.Image = mAcqFifo.Acquire(out triggerNum);
+
+                acqCount++;
+                if (acqCount >= 5)
+                {
+                    GC.Collect(); // 메모리 정리
+                    acqCount = 0;
+                }
+            }
+            catch (CogException cogEx)
+            {
+                MessageBox.Show("이미지 획득 중 오류 발생 : " + cogEx.Message);
+            }
+
+        } // 1회 촬영 버튼을 클릭한 경우
+
+        private void LiveOn_Btn(object sender, EventArgs e)
+        {
+            if (cogDisplay1.LiveDisplayRunning)
+            {
+                Acq_Once.Enabled = true;
+                SetControlEnabled(false);
+
+                // 라이브 디스플레이 정지
+                cogDisplay1.StopLiveDisplay();
+            }
+            else
+            {
+                Acq_Once.Enabled = false; 
+                SetControlEnabled(true);
+
+                // 라이브 디스플레이 시작
+                cogDisplay1.StartLiveDisplay(mAcqFifo);
+            }
+        } // Live On 버튼 클릭 시
+
+
+        //==================================================================================================
+        // 콤보 박스 & 속성 값 변경 시 이벤트
+        private void VideoFormatCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // 선택된 비디오 포맷으로 AcqFifo 생성
+            selectedFormat = VideoFormatCombo.SelectedItem.ToString();
+
+            CreateAcqFifoWithParams(selectedFormat);
+            UpdateCameraParamsUI();
+
+            Acq_Once.Enabled = true;
+
+            cogDisplay1.StartLiveDisplay(mAcqFifo);
+
+            // 컨트롤 상태 변경
+            SetControlEnabled(true);
+            Acq_Once.Enabled = false;
+        } // 비디오 포맷 콤보박스 변경될 때 호출되는 이벤트 핸들러
+
+        private void exposureUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            if (mExposure == null) return;
+
+            if (exposureUpDown.Value <= 1000 && exposureUpDown.Value >= 0)
+            {
+                mExposure.Exposure = (double)exposureUpDown.Value;
+            }
+            else
+            {
+                exposureUpDown.Value = 8M;
+                mExposure.Exposure = 8;
+            }
+        } // 노출 값이 변경될 때 호출되는 이벤트 핸들러
+
+        private void brightnessUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            if (mBrightness == null) return;
+
+            if (brightnessUpDown.Value <= 1 && brightnessUpDown.Value >= 0)
+            {
+                mBrightness.Brightness = (double)brightnessUpDown.Value;
+            }
+            else
+            {
+                brightnessUpDown.Value = 0.06M;
+                mBrightness.Brightness = 0.06;
+            }
+        } // 밝기 값이 변경될 때 호출되는 이벤트 핸들러
+
+        private void contrastUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            if (mContrast == null) return;
+
+            if (contrastUpDown.Value <= 1 && contrastUpDown.Value >= 0)
+            {
+                mContrast.Contrast = (double)contrastUpDown.Value;
+            }
+            else   
+            {
+                contrastUpDown.Value = 0.2M;
+                mContrast.Contrast = 0.2;
+            }
+        } // 대비 값이 변경될 때 호출되는 이벤트 핸들러 
+
+
+        //==================================================================================================
+        // 자원 해제
+        //private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        //{
+        //    // LiveDisplay가 실행 중이면 중지
+        //    if (cogDisplay1.LiveDisplayRunning)
+        //    {
+        //        cogDisplay1.StopLiveDisplay();
+        //    }
+        //} // 폼 종료 이전에 실행
+
+        //private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        //{
+        //    try
+        //    {
+        //        // AcqFifo 해제
+        //        if (mAcqFifo is IDisposable disposable)
+        //        {
+        //            disposable.Dispose();
+        //            mAcqFifo = null;
+        //        }
+
+        //        // 프레임그래버 연결 해제
+        //        mFrameGrabber?.Disconnect(false);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show("폼 종료 중 오류 발생 : " + ex.Message);
+        //    }
+
+        //} // 폼 종료 시 
+
+
+        public ICogAcqFifo PublicAcqFifo => mAcqFifo;            // FIFO 접근용
+        //public ICogFrameGrabber PublicFrameGrabber => mFrameGrabber; // FrameGrabber 접근용
+
+    }
+}
